ee49a1afc143c9584189a087f4dee97598f735b4
put your github username here <muathmhawich1@gmail.com>
2024-04-28 05:44:34 +0300

    fix lab15

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# D-Movies
+# Movies-Library
+Add Documentation in your README file
+# Project Name - Project Version
+Movies-Library -V1
+
+**Author Name**:Muath Mhawich
+
+## WRRC
+![Image Alt Text](./WRRC.png)
+
+## Overview
+This project is a simple Movie Data API built using Node.js and Express. It provides endpoints to retrieve movie data in JSON format.
+## Getting Started
+clone the repository to your local machine.
+Navigate to the project directory
+Install dependencies like 'npm install'
+Start the server 'npm start'
+
+## Project Features
+Retrieve movie data: The API provides an endpoint to retrieve movie data in JSON format.
+Error handling: The API includes error handling middleware to handle server errors (status 500) and "page not found" errors (status 404).
+## What observations or questions do you have about what you’ve learned so far?
+API Key Handling: It's crucial to securely handle API keys, whether by using environment variables or other secure methods. In this case, the error with the 401 status code suggests there might be an issue with how the API key is being used or passed to the API.
+Error Handling: Effective error handling is essential in any application, especially when making API requests. It ensures that errors are properly caught and handled, preventing the application from crashing or exposing sensitive information.
+Understanding API Responses: It's important to understand the structure of API responses and how to parse the data appropriately. This includes handling different status codes and response formats to provide meaningful feedback to users.
+Routing and Endpoint Design: Designing clear and logical routes and endpoints is essential for building a well-structured API. This includes considering RESTful principles and organizing routes in a logical manner.
+Testing: Testing APIs thoroughly is essential to catch potential issues early on. This includes both unit tests for individual functions and integration tests to ensure that the API endpoints function correctly as a whole.
+## How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+
+I wasn't keeping track of the exact time, but it probably took me around 30-40 minutes to complete this assignment, including reviewing and debugging the provided code, making necessary adjustments, and providing explanations and suggestio
---
+++ server.js
@@ -0,0 +1,185 @@
+const express = require('express');
+const fs = require('fs').promises;
+const axios = require('axios');
+const dotenv = require('dotenv').config();  
+const app = express();
+const pg=require('pg');
+const port = 3001;
+const apiKey = process.env.API_KEY; 
+  const DataBase=process.env.PG_DATABASE
+  const UserName=process.env.PG_USER
+  const password=process.env.PG_PASSWORD
+  const HOST=process.env.PG_HOST
+  const PORT=process.env.PG_PORT
+
+const client=new pg.Client(`postgressql://${UserName}:${password}@${HOST}:${PORT}/${DataBase}`);
+
+app.use(express.json());
+
+
+function Movie(data) {
+  this.title = data.title;
+  this.poster_path = data.poster_path;
+  this.overview = data.overview;
+}
+
+app.get('/movie',getMovies)
+app.post('/movie',addmovie)
+app.get('/trending', trendingHandler);
+app.get('/search', async (req, res) => {
+  const query = req.query.query;
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`);
+    const searchData = response.data.results.map(movie => ({
+      id: movie.id,
+      release_date: movie.release_date,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      overview: movie.overview
+    }));
+    res.json(searchData);
+  } catch (error) {
+    console.error('Error searching for movies:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//http://localhost:3001/movie/123
+app.get('/movie/:id', async (req, res) => {
+  const movieId = req.params.id;
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
+    const movieDetails = response.data;
+    res.json(movieDetails);
+  } catch (error) {
+    console.error('Error fetching movie details:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.get('/popular', async (req, res) => {
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+    const popularMovies = response.data.results.map(movie => ({
+      id: movie.id,
+      release_date: movie.release_date,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      overview: movie.overview
+    }));
+    res.json(popularMovies);
+  } catch (error) {
+    console.error('Error fetching popular movies:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const rawData = await fs.readFile('movies.json');
+    const moviesData = JSON.parse(rawData);
+    const movie = new Movie(moviesData);
+    res.json(movie);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/favorite', (req, res) => {
+  res.send('Welcome to Favorite Page'); 
+});
+
+app.put('/movie/:id',hundleUpdateMovie)
+app.delete ('/movie/:id',handleDeleteMovie);
+app.get('/movies/:id',handleSpesificMovie)
+
+function handleSpesificMovie(req,res){
+  let id=req.params.id;
+  const sql=`SELECT * FROM movie where id=${id}`
+  client.query(sql).then((result)=>{
+    res.status(200).json(result.rows);
+  }).catch((err)=>{
+    errorHandler(err)
+  })
+}
+
+function handleDeleteMovie(req,res){
+  let id=req.params.id;
+  const sql=`DELETE FROM movie where id=${id}` ;
+  client.query(sql).then((result)=>{
+    res.send('the data is deleted ');
+  }).catch((err)=>{
+     errorHandler(err);
+  })
+}
+
+function hundleUpdateMovie(req,res){
+  let id=req.params.id;
+  const { title, release_date, poster_path, overview } = req.body;
+  const sql=`UPDATE movie SET title = $1, release_date = $2, poster_path = $3, overview = $4 WHERE id = ${id} RETURNING *;`
+  let values=[title,release_date,poster_path,overview];
+  client.query(sql,values).then((result)=>{
+    res.status(200).send('the data is updated')
+  }).catch((err)=>{
+    errorHandler(err);
+  })
+}
+
+ 
+ function getMovies(req,res){
+  const sql='SELECT * from movie '
+  client.query(sql).then((data)=>{
+    res.send(data.rows)
+  }).catch((err)=>{
+    errorHandler(err,req,res);
+  })
+ }
+
+ function addmovie(req, res) {
+  const movie = req.body;
+  const sql = 'INSERT INTO movie (title, release_date, poster_path, overview) VALUES ($1, $2, $3, $4) RETURNING *';
+  const values = [movie.title, movie.release_date, movie.poster_path, movie.overview];
+  client.query(sql, values)
+      .then((data) => {
+          res.send('your data was added');
+      })
+      .catch((err) => {
+          errorHandler(err, req, res);
+      });
+}
+
+function trendingHandler(req, res) {
+  axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
+    .then(response => {
+      const trendingMovies = response.data.results.map(movie => ({
+        id: movie.id,
+        release_date: movie.release_date,
+        title: movie.title,
+        poster_path: movie.poster_path,
+        overview: movie.overview
+      }));
+      res.json(trendingMovies);
+    })
+    .catch(error => {
+      console.error('Error fetching trending movies:', error);
+      res.status(500).json({ error: error.message });
+    });
+}
+
+function errorHandler(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+}
+
+function notFoundHandler(req, res, next) {
+  res.status(404).send('Page Not Found');
+}
+
+app.use(errorHandler);
+app.use(notFoundHandler);
+client.connect().then(()=>{
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+})
